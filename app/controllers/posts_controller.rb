47f3f8e0441240e6d5084e3015ab6ec67e83b1e9
seanraad123 <seanconrad123@gmail.com>
2018-07-25 23:24:30 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,7 @@ class PostsController < ApplicationController
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(params.require(:post))
+		params.inspect
 	  redirect_to post_path(@post)
 	end
 
